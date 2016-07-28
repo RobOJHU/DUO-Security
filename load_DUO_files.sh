bf060b817644ac885fc635859475a5d71a063e24
Robert O'Steen <rosteen@jhu.edu>
2016-07-28 15:39:25 -0400

    Update load_DUO_files.sh

--- load_DUO_files.sh
+++ load_DUO_files.sh
@@ -20,13 +20,12 @@ cd /root
 # SAV file to be changed
 mv /etc/pam.d/system-auth /etc/pam.d/system-auth.ORG
 mv /etc/ssh/sshd_config /etc/ssh/sshd_config.SAV
-mv /etc/duo/pam_duo.conf /etc/duo/pam_duo.conf.SAV
 mv /etc/pam.d/sshd /etc/pam.d/sshd.ORG
 
 #go get them from GITHUB
 echo "go get the FILES needed from GITHUB"
 cd /etc/duo/
-getno https://raw.githubusercontent.com/RobOJHU/DUO-Security/master/pam_duo.conf
+
 chmod 600 ./pam_duo.conf
 cd /etc/pam.d/
 getno https://raw.githubusercontent.com/RobOJHU/DUO-Security/master/system-auth
@@ -42,6 +41,6 @@ service sshd restart
 echo "DOU install completed for a RHEL6 machine --RobO"
 echo "##########"
 echo "##########"
-echo "##########   you MUST remember to FIX the now obfuscated  /etc/duo/pam_duo.conf FILE ########"
+echo "##########   you MUST remember to FIX the now BLANK vim /etc/duo/pam_duo.conf FILE ########"
 echo "##########"
 echo "###########"
